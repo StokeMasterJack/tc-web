336a66c3b4a0f156a42938d3d89b49f56d4c1638
David Ford <dford@smart-soft.com>
2018-05-04 22:06:37 -0700

    Add Flutter

---
+++ client/src/Tmp.js
@@ -0,0 +1,9 @@
+import * as React from "react";
+
+function buttonBar({ x1, x2, x3 }) {
+  return <div>
+      <button>{x1}</button>
+      <button>{x2}</button>
+      <button>{x3}</button>
+    </div>
+}
